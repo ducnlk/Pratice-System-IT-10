9d94b488f60573e608161764cb42c965bd23a665
ducnlk <nguyenlekieuduc.w@gmail.com>
2024-08-07 11:10:13 +0700

    update

--- system/system/settings.py
+++ system/system/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d@a-@rtf$0npfeg!ng-%b40=5wh@+65*f!3kkea78omx+rasyk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['HeThongHoTroLTTinHoc10.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -116,13 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-# settings.py
+STATIC_URL = 'static/'
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'HeThongHoTroLT/static',
-]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
